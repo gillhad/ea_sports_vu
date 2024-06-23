0190ed25184962c4514e687c71f9b1c91b7aa512
CRISTIANB <crbaronv@gmail.com>
2024-06-23 15:59:46 +0200

    update players detail closes #19

--- src/app/views/player-details/player-details.component.ts
+++ src/app/views/player-details/player-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, Input, OnInit, inject } from '@angular/core';
 import { RequestPlayerService } from '../../services/request-player.service';
 import { Player } from '../../models/player';
 import { ActivatedRoute, Router } from '@angular/router';
@@ -6,22 +6,57 @@ import { ActivatedRoute, Router } from '@angular/router';
 @Component({
   selector: 'app-player-details',
   templateUrl: './player-details.component.html',
-  styleUrl: './player-details.component.scss'
+  styleUrl: './player-details.component.scss',
 })
-export class PlayerDetailsComponent implements OnInit{
-  playerRequest= inject(RequestPlayerService);
-router = inject(Router);
-activatedRoute = inject(ActivatedRoute)
-  players: Player[] = [];
-
+export class PlayerDetailsComponent implements OnInit {
+  playerRequest = inject(RequestPlayerService);
+  router = inject(Router);
+  activatedRoute = inject(ActivatedRoute);
+  @Input() player?:Player;
+  playerRandom?:Player;
+  
 
   ngOnInit(): void {
-  this.players = this.playerRequest.getPlayers();
-  console.log(this.players);
-}
+    this.player = new Player({
+      "name": "Lionel Messi",
+      "image1": "",
+      "image2": "",
+      "stats": [
+        {
+          "name": "pace",
+          "value": 80
+        },
+        {
+          "name": "shooting",
+          "value": 87
+        },
+        {
+          "name": "passing",
+          "value": 90
+        },
+        {
+          "name": "dribling",
+          "value": 94
+        },
+        {
+          "name": "defending",
+          "value": 33
+        },
+        {
+          "name": "physycal",
+          "value": 64
+        }
+      ],
+      "media": [
+        "https://www.youtube.com/embed/K7ylwMki3mw?si=OGdGcr9Js2Z68_p0",
+        "https://www.youtube.com/embed/2zfzaoioxQg?si=mx8AXKi6sRaCUPXB",
+        "https://www.youtube.com/embed/rQ7IWWKlPxA?si=zK-p1k6JsCAutSxi",
+        "https://www.youtube.com/embed/MEWSjzw2jVw?si=Tbttp5wLzKBcoWDo"
+      ]
+    })
+  }
 
-navigate(){
-this.router.navigate(["videos"],{relativeTo:this.activatedRoute})
-}
-
+  navigate() {
+    this.router.navigate(['videos'], { relativeTo: this.activatedRoute });
+  }
 }
