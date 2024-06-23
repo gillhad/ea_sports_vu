32b7c74188f264d2312012a6d39f03a1bc426c59
LidiaLuque <lidialg.dev@gmail.com>
2024-06-23 19:19:43 +0200

    fixed footer and created view not found closes #25

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './views/home/home.component';
 import { PlayerDetailsComponent } from './views/player-details/player-details.component';
 import { VideoDetailsComponent } from './views/video-details/video-details.component';
+import { NotFoundComponent } from './views/not-found/not-found.component';
 
 const routes: Routes = [
   {
@@ -39,7 +40,7 @@ const routes: Routes = [
     
   },
 
-  { path: '**', component: HomeComponent },
+  { path: '**', component: NotFoundComponent },
 ];
 
 @NgModule({
